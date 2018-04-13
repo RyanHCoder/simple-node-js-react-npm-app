c97c8c505d04d909de45bfcbd1d6e2d61c65fece
Ryan Hatchard <w0296466@nscc.ca>
2018-04-12 22:33:48 -0300

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+	<h1>Custom message: {this.props.message}</h1>
       </div>
     );
   }
